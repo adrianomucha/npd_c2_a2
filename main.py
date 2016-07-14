a096c8cd80ce566fe12edd33497471126ebcbe70
Adrian <adrianomucha@gmail.com>
2016-07-14 19:10:41 +0000

    new updated version

--- main.py
+++ main.py
@@ -53,21 +53,53 @@ class Triangle(object):
 
     def update_edge_length(self, new_length):
         self.edge_length = new_length
+
+    def add_ally(self, shape_object):
+        self.allies.append(shape_object)
+
+    def add_enemy(self, shape_object):
+        self.enemies.append(shape_object)
+
+    def edge_height(self, new_height):
+        self.edge_height = new_height
+
+class Circle(object):
+
+    def __init__(self, radius, pi, name, allies, enemies):
+        self.radius = radius
+        self.pi = pi
+        self.name = name
+        self.allies = allies
+        self.enemies = enemies
+
+    def area(self):
+        return self.pi * self.radius * self.radius
+
+    def perimeter(self):
+        return self.radius * 1
+   # def update_radius(self, new_radius):
+    #    self.radius.append(shape_object)
     def add_ally(self, shape_object):
         self.allies.append(shape_object)
     def add_enemy(self, shape_object):
         self.enemies.append(shape_object)
-    def edge_height(self, new_height):
-        self.edge_height = new_height
+
+
 
 if __name__=='__main__':
-    square_marty = Square(5, "marty", [], [])
+    square_marty = Square(5, "marty", [], [triangle_tom, circle_andrew])
     print("Area: ",square_marty.area())
     print("Update Edge: ",square_marty.update_edge_length(10))
     print("Area: ",square_marty.area())
 
 if __name__ == '__main__':
-    triangle_tom = Triangle(10, 3, "tom", [], [])
+    triangle_tom = Triangle(2, 2 , "tom", [], [square_marty])
     print("Area:",triangle_tom.area())
     print("Update Edge: ",triangle_tom.update_edge_length(10))
     print("Area: ",triangle_tom.area())
+
+if __name__ == '__main__':
+    circle_andrew = Circle(2,3, "Andrew",[triangle_tom], [square_marty])
+    print("Area:", circle_andrew.area())
+    #print("Update Edge: ", circle_andrew.update_radius(5))
+    print("Area: ", circle_andrew.area())
